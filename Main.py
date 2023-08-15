99c4105220663fc35b3e118e9613670fd66e631f
Dan Crites <dcriteswv@gmail.com>
2023-08-14 22:05:31 -0400

    working on distance table

--- Main.py
+++ Main.py
@@ -3,6 +3,8 @@
 # NHP2 — NHP2 TASK 1: WGUPS ROUTING PROGRAM
 
 import csv
+import datetime
+
 import HashTable
 
 import CSV_FILES
@@ -42,35 +44,35 @@ with open('CSV_FILES/WGUPS_Distance_Table.csv') as csv_file_distance:
         listOfCurrentRoute.append(column[col])
     index = listOfCurrentRoute.index(min(listOfCurrentRoute))
     index += 2
-    # print("index ", index)
-    # listOfRoutesUsingNN.append(index)
-    # print("Current Route", listOfCurrentRoute)
-    #
-    # print("List of routes", listOfRoutesUsingNN)
-    # listOfCurrentRoute.clear()
+    print("index ", index)
+    listOfRoutesUsingNN.append(index)
+    print("Current Route", listOfCurrentRoute)
+
+    print("List of routes", listOfRoutesUsingNN)
+    listOfCurrentRoute.clear()
 
     for col2 in CSV_Distance:
         listOfCurrentRoute.append(col2[index])
-    # listOfCurrentRoute = [ele.lstrip('0') for ele in listOfCurrentRoute]
-    # listOfCurrentRoute = list(filter(None, listOfCurrentRoute))
+    listOfCurrentRoute = [ele.lstrip('0') for ele in listOfCurrentRoute]
+    listOfCurrentRoute = list(filter(None, listOfCurrentRoute))
 
-    # listOfCurrentRoute = ['0' if i.strip() == '' else i for i in listOfCurrentRoute]
-    # print("Current List", listOfCurrentRoute)
-    #
-    # index = listOfCurrentRoute.index(min(listOfCurrentRoute))
-    #
-    # print("index 2 ", index)
-    #
-    # listOfRoutesUsingNN.append(index)
-    # print("List of cur ", listOfRoutesUsingNN)
+    listOfCurrentRoute = ['0' if i.strip() == '' else i for i in listOfCurrentRoute]
+    print("Current List", listOfCurrentRoute)
+
+    index = listOfCurrentRoute.index(min(listOfCurrentRoute))
+
+    print("index 2 ", index)
+
+    listOfRoutesUsingNN.append(index)
+    print("List of cur ", listOfRoutesUsingNN)
 
 with open('CSV_FILES/WGUPS_Package_File.csv') as csv_file_package:
     CSV_Package = csv.reader(csv_file_package)
     CSV_Package = list(CSV_Package)
 
-
-
 myHash = ChainingHashTable()
+
+
 def loadPackageData(fileName):
     with open(fileName, encoding='utf-8-sig') as packageData:
         PO = csv.reader(packageData, delimiter=',')
@@ -87,12 +89,35 @@ def loadPackageData(fileName):
 
             package = package
 
-            myHash.insert(ID,package)
+            myHash.insert(ID, package)
 
 
 loadPackageData('CSV_FILES/WGUPS_Package_File.csv')
 
-for i in range(len(myHash.table) + 1):
-    print("Package ID: {} {}".format(i+1, myHash.search(i+1)))
+# for i in range(len(myHash.table) + 1):
+#     print("Package ID: {} {}".format(i + 1, myHash.search(i + 1)))
+#
+# print("yo", myHash.search(1))
 
-print("yo", myHash.search(1))
+truck1 = [1, 2, 4, 5, 7, 8, 10, 11, 12, 21, 22, 23, 24, 26, 27, 29]
+truck2 = [3, 18, 36, 38, 13, 14, 15, 16, 17, 19, 20, 30, 31, 33, 34, 35]
+truck3 = [6, 25, 28, 32, 37, 39, 40, 9]
+
+loadTruck1 = Truck.Truck(16, 18, 1, truck1, 0.0, "4001 South 700 East", datetime.timedelta(hours=8))
+
+loadTruck2 = Truck.Truck(16, 18, 2, truck2, 0.0, "4001 South 700 East", datetime.timedelta(hours=8))
+
+loadTruck3 = Truck.Truck(16, 18, 3, truck3, 0.0, "4001 South 700 East", datetime.timedelta(hours=10, minutes=20))
+
+print("yo ", loadTruck3.packages)
+
+
+def distance_in_between(x_value, y_value):
+    distance = CSV_Distance[x_value][y_value]
+    if distance == '':
+        distance = CSV_Distance[y_value][x_value]
+
+    print("Distance ", distance)
+
+
+distance_in_between(6, 2)
