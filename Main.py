ea6c583d2b9a8e3ebf0d93f09fa7702d7763a69d
Dan Crites <dcriteswv@gmail.com>
2023-08-22 23:24:06 -0400

    working on delivery process

--- Main.py
+++ Main.py
@@ -28,43 +28,6 @@ with open('CSV_FILES/WGUPS_Distance_Table.csv', encoding="utf-8-sig") as csv_fil
     CSV_Distance = csv.reader(csv_file_distance)
     CSV_Distance = list(CSV_Distance)
 
-    # rows = len(CSV_Distance)
-    # cols = len(CSV_Distance[0])
-
-    # row = 0
-    # col = 0
-    # index = 0
-
-    # listOfCurrentRoute = []
-    # listOfRoutesUsingNN = []
-
-    # while (col <= cols):
-
-    # for column in CSV_Distance:
-    #     listOfCurrentRoute.append(column[col])
-    # index = listOfCurrentRoute.index(min(listOfCurrentRoute))
-    # index += 2
-    # print("index ", index)
-    # listOfRoutesUsingNN.append(index)
-    # print("Current Route", listOfCurrentRoute)
-    #
-    # print("List of routes", listOfRoutesUsingNN)
-    # listOfCurrentRoute.clear()
-    #
-    # for col2 in CSV_Distance:
-    #     listOfCurrentRoute.append(col2[index])
-    # listOfCurrentRoute = [ele.lstrip('0') for ele in listOfCurrentRoute]
-    # listOfCurrentRoute = list(filter(None, listOfCurrentRoute))
-    #
-    # listOfCurrentRoute = ['0' if i.strip() == '' else i for i in listOfCurrentRoute]
-    # print("Current List", listOfCurrentRoute)
-    #
-    # index = listOfCurrentRoute.index(min(listOfCurrentRoute))
-    #
-    # print("index 2 ", index)
-    #
-    # listOfRoutesUsingNN.append(index)
-    # print("List of cur ", listOfRoutesUsingNN)
 
 with open('CSV_FILES/WGUPS_Package_File.csv', encoding='utf-8-sig') as csv_file_package:
     CSV_Package = csv.reader(csv_file_package)
@@ -104,9 +67,7 @@ truck2 = [3, 18, 36, 38, 13, 14, 15, 16, 17, 19, 20, 30, 31, 33, 34, 35]
 truck3 = [6, 25, 28, 32, 37, 39, 40, 9]
 
 loadTruck1 = Truck.Truck(16, 18, 1, truck1, 0.0, "4001 South 700 East", datetime.timedelta(hours=8))
-
 loadTruck2 = Truck.Truck(16, 18, 2, truck2, 0.0, "4001 South 700 East", datetime.timedelta(hours=8))
-
 loadTruck3 = Truck.Truck(16, 18, 3, truck3, 0.0, "4001 South 700 East", datetime.timedelta(hours=10, minutes=20))
 
 
@@ -119,69 +80,64 @@ def distance_in_between(x_value, y_value):
 
 
 def delivery_process(truck):
+
     current_address = truck.address
 
     empty_list = []
     column_distant_list = []
+    addresses_for_packages = []
 
+    truck_mileage = 0.0
     shortest_route = 0
-    truck_mileage = 0
     index = 0
-
-    sm = float('inf')
-    ssm = float('inf')
-
-    # Adding row 0 to a list
-    row_distant_list = CSV_Distance.pop(0)
-
-    # Adding column 0 to column_distant_list
-    for column in CSV_Distance:
-        column_distant_list.append(column[0])
-
-    # Extending row_distant_list list with column_distant_list
-    row_distant_list.extend(column_distant_list)
-
-    # Removing empty spaces and moving data to 'cleaned list'
-    cleaned_list = [ele for ele in row_distant_list if ele.strip()]
-
-    # Finding the index of the second smallest number
-
-    for item in cleaned_list[1:]:
-        if shortest_route == 0 or shortest_route > item:
-            shortest_route = item
-            index = cleaned_list.index(item)
-
-    # WORKING HERE!!!!!!!!!
-    selmt = min(cleaned_list)
-    cleaned_list.pop(cleaned_list.index(selmt))
-    sselmt = min(cleaned_list)
-    truck_mileage = float(sselmt)
-
-
-    print("ASDA DAD AS D ", truck_mileage)
-    # for num in cleaned_list:
-    #     if num <= sm:
-    #         sm, ssm = num, sm
-    #     elif num < ssm:
-    #         ssm = num
-
-    print("Index ", index)
-    print("Second lowest num ", shortest_route)
-    print("Truck Mileage ", truck_mileage)
-
-    print("List Of Current Route ", cleaned_list)
-
-    print("Current Address ", current_address)
+    count = 0
 
     for packageID in truck.packages:
         package_list = myHash.search(packageID)
+        addresses_for_packages.append(package_list[0])
         empty_list.append(package_list)
 
+    print("Here ", empty_list)
+    del empty_list[0]
+    print("Here ", empty_list)
     print("Inside package list", empty_list)
+    print("Addresses ", addresses_for_packages)
+    # for address_index in CSV_Address:
+
+    while count == 0:
+        count += 1
+        # Adding row 0 to a list
+        row_distant_list = CSV_Distance.pop(index)
+
+        # Adding column 0 to column_distant_list
+        for column in CSV_Distance:
+            column_distant_list.append(column[0])
+
+        # Extending row_distant_list list with column_distant_list
+        row_distant_list.extend(column_distant_list)
+
+        # Removing empty spaces and moving data to 'cleaned list'
+        cleaned_list = [ele for ele in row_distant_list if ele.strip()]
+
+
+        # Finding the index of the second-smallest number
+        for item in cleaned_list[1:]:
+            if shortest_route == 0 or shortest_route > item:
+                shortest_route = item
+                index = cleaned_list.index(item)
+                mileage = float(shortest_route)
+
+        truck_mileage += mileage
+        print("Mileage ", truck_mileage)
+        # print("Index ", index)
+        # print("Second lowest num ", shortest_route)
+        # print("Truck Mileage ", truck_mileage)
+        #
+        # print("List Of Current Route ", cleaned_list)
+        # print("Current Address ", current_address)
 
 
 delivery_process(loadTruck1)
-distance_in_between(0, 1)
 
 # Garbage Code
 # index = listOfCurrentRoute.index(min(listOfCurrentRoute))
@@ -195,3 +151,51 @@ distance_in_between(0, 1)
 # for index, value in enumerate(CSV_Package):
 #     my_dict[index + 1] = value
 # print("my dict", my_dict)
+
+# selmt = min(cleaned_list)
+# cleaned_list.pop(cleaned_list.index(selmt))
+# sselmt = min(cleaned_list)
+# truck_mileage = float(sselmt)
+# for num in cleaned_list:
+#     if num <= sm:
+#         sm, ssm = num, sm
+#     elif num < ssm:
+#         ssm = num
+
+# rows = len(CSV_Distance)
+# cols = len(CSV_Distance[0])
+
+# row = 0
+# col = 0
+# index = 0
+
+# listOfCurrentRoute = []
+# listOfRoutesUsingNN = []
+
+# while (col <= cols):
+
+# for column in CSV_Distance:
+#     listOfCurrentRoute.append(column[col])
+# index = listOfCurrentRoute.index(min(listOfCurrentRoute))
+# index += 2
+# print("index ", index)
+# listOfRoutesUsingNN.append(index)
+# print("Current Route", listOfCurrentRoute)
+#
+# print("List of routes", listOfRoutesUsingNN)
+# listOfCurrentRoute.clear()
+#
+# for col2 in CSV_Distance:
+#     listOfCurrentRoute.append(col2[index])
+# listOfCurrentRoute = [ele.lstrip('0') for ele in listOfCurrentRoute]
+# listOfCurrentRoute = list(filter(None, listOfCurrentRoute))
+#
+# listOfCurrentRoute = ['0' if i.strip() == '' else i for i in listOfCurrentRoute]
+# print("Current List", listOfCurrentRoute)
+#
+# index = listOfCurrentRoute.index(min(listOfCurrentRoute))
+#
+# print("index 2 ", index)
+#
+# listOfRoutesUsingNN.append(index)
+# print("List of cur ", listOfRoutesUsingNN)
